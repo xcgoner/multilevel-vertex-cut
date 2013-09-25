833de8498e717bde14b55161a14304e3596959ec
xcgoner1108@gmail.com <xcgoner1108@gmail.com@484c4b01-470f-9c83-6c9b-1340214dcba6>
2013-09-25 09:06:36 +0000

    random assign to threads by : edgernd(gen) % (nthreads[i])

--- oblivious-streaming-random-thread/GraphPartitioner/partition_strategy.hpp
+++ oblivious-streaming-random-thread/GraphPartitioner/partition_strategy.hpp
@@ -474,7 +474,8 @@ namespace graphp {
 					// random assign
 					//const edge_pair_type edge_pair(min(e.source, e.target), max(e.source, e.target));
 					basic_graph::part_t assignment;
-					assignment = edge_counter % (nthreads[i]);
+					//assignment = edge_counter % (nthreads[i]);
+					assignment = edgernd(gen) % (nthreads[i]);
 					e.placement = assignment;
 					thread_p[assignment]++;
 					edge_counter++;
